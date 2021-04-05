a0bc2c2169184d56da22a4ad48c34e3d715a38f0
Taimaz Hafezi <taimazhafezi@gmail.com>
2021-04-04 21:31:50 -0400

    Update LAB3
    Update LAB3

--- PowerShell/ipconfigreport.ps1
+++ PowerShell/ipconfigreport.ps1
@@ -1,1 +1,1 @@
-﻿Get-CimInstance Win32_NetworkAdapterConfiguration | where-object IPEnabled -EQ "True" | Select-Object Description, Index, IPAddress, IPSubnet, DNSDomain, DNSHostName | Format-Table
+﻿Get-CimInstance Win32_NetworkAdapterConfiguration | where-object IPEnabled -EQ "True" | Select-Object Description, Index, IPAddress, IPSubnet, DNSDomain, DNSHostName | Format-table
