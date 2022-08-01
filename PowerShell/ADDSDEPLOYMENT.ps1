03621a910c61033ddb3981118d633c292b04b91e
Taimaz Hafezi <68306895+TaimazHafezi@users.noreply.github.com>
2022-08-01 17:39:20 -0400

    AD DS Deployment Script
    AD DS Deployment Script Written by Taimaz Hafezi

---
+++ PowerShell/ADDSDEPLOYMENT.ps1
@@ -0,0 +1,16 @@
+﻿# Windows powershell Script for AD DS Deployment
+
+Import Module ADDSDeployment 
+install -ADDSForest 
+-CreateDnsDelegation:$false 
+-DatabasePath "C:\Windows\NTDS" 
+-DomainMode "WinThreshold"
+-DomainName "taimaz.lab"
+-DomainNetbiosName "TAIMAZ"
+-ForestMode "WinThreshold"
+-InstallDns:$true
+-LogPath "C:\Windows\NTDS"
+-NoRebootOnCompletion:$false
+-sysvolpath "C:\Windows\SYSVOL"
+-Force:$true
+
